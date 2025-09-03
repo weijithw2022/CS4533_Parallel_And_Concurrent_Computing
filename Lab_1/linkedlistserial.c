c6a7f075f744bd5f2b9a76f076bd793fa9ac9e98
weijithw2022 <weijithwimalasiri2020@gmail.com>
2025-09-03 23:53:51 +0530

    Calculate required runs for mentioned accuracy and confidence level and get execution time  mean and std

--- Lab_1/linkedlistserial.c
+++ Lab_1/linkedlistserial.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 struct list_node_s {
    int data;
@@ -80,38 +81,8 @@ void FreeList(struct list_node_s* head_p){
     }
 }
 
-int main(int argc, char* argv[])
-{
-    if(argc != 6){
-        fprintf(stderr, "Usage: %s <n> <m> <mMember> <mInsert> <mDelete>\n", argv[0]);
-        return 1;
-    }
-
-    int n = atoi(argv[1]);
-    int m = atoi(argv[2]);
-    double mMember = atof(argv[3]);
-    double mInsert = atof(argv[4]);
-    double mDelete = atof(argv[5]);
-
-    if(n <= 0 || n > 65536){
-        fprintf(stderr, "n must be between 1 and 2^16\n");
-        return 1;
-    }
-
-    if(m<=0){
-        fprintf(stderr, "m must be greater than 0\n");
-        return 1;
-    }
-
-    if(mMember + mInsert + mDelete > 1.0001 || mMember + mInsert + mDelete < 0.9999){
-        fprintf(stderr, "Fraction sum must be equal to 1\n");
-        return 1;
-    }
-
-    
+double run_experiments(int n, int m, double mMember, double mInsert, double mDelete){
     struct list_node_s* head = NULL;
-
-    srand(time(NULL));
     
     int count = 0;
     while(count < n){
@@ -119,9 +90,6 @@ int main(int argc, char* argv[])
         if(Insert(value, &head)) /*Fills with n unique values*/
             count++;
     }
-
-    // printf("List after filling with unique values: \n");
-    // PrintList(head);
 
     int memberfns = 0, insertfns = 0, deletefns = 0;
     int totalmemberfns = m * mMember;
@@ -158,6 +126,9 @@ int main(int argc, char* argv[])
         ops[j] = temp;
     }
 
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     for(int i=0;i<m;i++){
         if(ops[i]=='M'){
             int val = rand() % 65536;
@@ -172,93 +143,102 @@ int main(int argc, char* argv[])
         }
     }
 
-    // while(succesCount < m){
-    //     double prob = (double) rand() / RAND_MAX;
-    //     int value = rand() % 65536;
-    //     if(prob < mMember){
-    //         Member(value, head);
-    //         memberfns++;
-    //         succesCount++;
-    //     }
-    //     else if(prob < mMember + mInsert){
-    //         if(Insert(value, &head)){
-    //             insertfns++;
-    //             succesCount++;
-    //         }
-    //     }
-    //     else{
-    //         if(Delete(value, &head)){
-    //             deletefns++;
-    //             succesCount++;
-    //         }
-    //         // Delete(value, &head);
-    //         // deletefns++;
-    //     }
-    // }
-    
-    /* for(int i =0; i<m; i++){
-        double prob = (double) rand() / RAND_MAX;
-        int value = rand() % 65536;
-        if(prob < mMember){
-            Member(value, head);
-            memberfns++;
-        }
-        else if(prob < mMember + mInsert){
-            if(Insert(value, &head))
-                insertfns++;
-        }
-        else{
-            if(Delete(value, &head))
-                deletefns++;
-            // Delete(value, &head);
-            // deletefns++;
-        }
-    } */
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    // printf("Wall time used: %f seconds\n", elapsed);
 
-    printf("Number of member functions: %d\n", memberfns);
-    printf("Number of insert functions: %d\n", insertfns);
-    printf("Number of delete functions: %d\n", deletefns);
-
-    printf("List after all operations: \n");
-    PrintList(head);
-
-    // Insert(5, &head);
-    // Insert(3, &head);
-    // Insert(7, &head);
-
-    // printf("List after inserts: \n");
+    // printf("List after all operations: \n");
     // PrintList(head);
 
-
-    // Insert(4, &head);
-    // Insert(6, &head);
-    // Insert(2, &head);
-    // printf("List after more inserts: \n");
-    // PrintList(head);
-    // Insert(5, &head);
-    // printf("List after inserting 5: \n");
-    // PrintList(head);
-
-    // int ans = Member(5, head);
-    // printf("Is 5 a member of the list? %s\n", ans ? "Yes" : "No");
-
-    // Delete(5, &head);
-    // Delete(7, &head);
-    // Delete(4, &head);
-    // printf("List after deletes: \n");
-    // PrintList(head);
-    // int ans2 = Member(4, head);
-    // printf("Is 4 a member of the list? %s\n", ans2 ? "Yes" : "No");
-    // Delete(6, &head);
-    // Delete(2, &head);
-    // printf("List after deleting: \n");
-    // PrintList(head);
-    // printf("Trying to delete from the empty list: \n");
-    // Delete(2, &head);
 
     FreeList(head);
     free(insertNewValues);
     free(deleteNewValues);
+    free(ops);
+    return elapsed;
+}
 
+
+
+int main(int argc, char* argv[])
+{
+    if(argc != 6){
+        fprintf(stderr, "Usage: %s <n> <m> <mMember> <mInsert> <mDelete>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    double mMember = atof(argv[3]);
+    double mInsert = atof(argv[4]);
+    double mDelete = atof(argv[5]);
+
+    if(n <= 0 || n > 65536){
+        fprintf(stderr, "n must be between 1 and 2^16\n");
+        return 1;
+    }
+
+    if(m<=0){
+        fprintf(stderr, "m must be greater than 0\n");
+        return 1;
+    }
+
+    if(mMember + mInsert + mDelete > 1.0001 || mMember + mInsert + mDelete < 0.9999){
+        fprintf(stderr, "Fraction sum must be equal to 1\n");
+        return 1;
+    }
+
+    int sample_runs = 30;
+    double sample_times[sample_runs];
+    
+    for(int i= 0; i< sample_runs; i++){
+        srand(time(NULL) + i);
+        sample_times[i] = run_experiments(n, m, mMember, mInsert, mDelete);
+    }
+
+    double sample_sum = 0.0;
+    for(int i=0; i<sample_runs; i++){
+        sample_sum += sample_times[i];
+    }
+    double sample_mean = sample_sum / sample_runs;
+
+    double variance = 0.0;
+    for(int i=0; i<sample_runs; i++){
+        variance += (sample_times[i] - sample_mean) * (sample_times[i] - sample_mean);
+    }
+    variance /= (sample_runs-1);
+    double sample_stddev = sqrt(variance);
+
+    double r = 5.0; // ±5% accuracy
+    double z = 1.96; // 95% confidence level
+
+    double required_runs = pow((100 * z * sample_stddev) / (r * sample_mean), 2);
+    int required_samples = (int)ceil(required_runs);
+    printf("Required runs: %f\n", required_runs);
+    printf("Required samples: %d\n", required_samples);
+
+    double *all_times = malloc(required_samples * sizeof(double));
+
+    for(int i=0; i<required_samples; i++){
+        srand(time(NULL) + i);
+        all_times[i] = run_experiments(n, m, mMember, mInsert, mDelete);
+    }
+
+    double all_sum = 0.0;
+    for(int i=0; i<required_samples; i++){
+        all_sum += all_times[i];
+    }
+    double average = all_sum / required_samples;
+
+    double all_variance = 0.0;
+    for(int i=0; i<required_samples; i++){
+        all_variance += (all_times[i] - average) * (all_times[i] - average);
+    }
+    all_variance /= (required_samples);
+    double all_stddev = sqrt(all_variance);
+
+    printf("All runs - Mean: %f, StdDev: %f\n", average, all_stddev);
+
+    free(all_times);
     return 0;
 }
